94583dfd21c8007b7cf5bafcaebdcd548fe4f285
Isela <isela.v.alarcon@gmail.com>
2021-05-19 23:22:38 +0200

    Communicating the items prop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a fron end javascript library"
+  },
+  {
+    title: "Why use React",
+    content: "React is a favorite JS library among engineers"
+  },
+  {
+    title: "How do you use React",
+    content: "I use React by creating components"
+  }
+];
 
 export default () => {
-  return <h1>Widgets App</h1>;
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
 };
