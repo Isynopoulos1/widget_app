9c5a4ec32192fe664819184ea05f5be04464309e
Isela <isela.v.alarcon@gmail.com>
2021-05-25 17:41:59 +0200

    Text inputs with hooks part 1

--- src/App.js
+++ src/App.js
@@ -5,22 +5,23 @@ import Search from "./components/Search";
 const items = [
   {
     title: "What is React?",
-    content: "React is a front end javascript library"
+    content: "React is a front end javascript framework"
   },
   {
-    title: "Why use React",
+    title: "Why use React?",
     content: "React is a favorite JS library among engineers"
   },
   {
-    title: "How do you use React",
-    content: "I use React by creating components"
+    title: "How do you use React?",
+    content: "You use React by creating components"
   }
 ];
 
-export default () => {
+const App = () => {
   return (
     <div>
       <Search />
     </div>
   );
 };
+export default App;
