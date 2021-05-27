2274bdd18607f21fc36b9a7840ff2dc42f759bdb
Isela <isela.v.alarcon@gmail.com>
2021-05-27 15:54:53 +0200

    Executing the request from use Effect

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,7 +6,15 @@ const Search = () => {
 
   useEffect(() => {
     const search = async () => {
-      await axios.get("sdakhakd");
+      await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term
+        }
+      });
     };
 
     search();
