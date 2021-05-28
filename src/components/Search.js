50e006659d808cfefa7ed66727bafb55f6ca04ef
Isela <isela.v.alarcon@gmail.com>
2021-05-28 10:33:13 +0200

    List building

--- src/components/Search.js
+++ src/components/Search.js
@@ -24,6 +24,17 @@ const Search = () => {
     search();
   }, [term]);
 
+  const renderedResults = results.map(result => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{results.title}</div>
+          {result.snippet}
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <div className="ui form">
@@ -36,6 +47,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
