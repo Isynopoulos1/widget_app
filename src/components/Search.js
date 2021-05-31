66f5736ddf86c32b1890e6242c1cd376294eb0b8
Isela <isela.v.alarcon@gmail.com>
2021-05-31 14:25:25 +0200

    Implementing a delayed request

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,10 +9,27 @@ const Search = () => {
   //Use effect cleanup function
 
   useEffect(() => {
-    console.log("Initial render on term was changed");
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term
+        }
+      });
 
+      setResults(data.query.search);
+    };
+
+    const timeoutId = setTimeout(() => {
+      if (term) {
+        search();
+      }
+    }, 500);
     return () => {
-      console.log("CLEANUP");
+      clearTimeout(timeoutId);
     };
   }, [term]);
 
