51ba302d365f272590457cb8dd3c156b8e9cf706
Isela <isela.v.alarcon@gmail.com>
2021-06-08 22:10:16 +0200

    searching on initial render

--- src/components/Search.js
+++ src/components/Search.js
@@ -23,14 +23,18 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    const timeoutId = setTimeout(() => {
-      if (term) {
-        search();
-      }
-    }, 500);
-    return () => {
-      clearTimeout(timeoutId);
-    };
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 1000);
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
   }, [term]);
 
   const renderedResults = results.map(result => {
