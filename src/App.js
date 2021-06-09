6b2a78406b92e27041565ceee905484744518c82
Isela <isela.v.alarcon@gmail.com>
2021-06-09 23:03:40 +0200

    Scafolding the dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
@@ -17,11 +18,24 @@ const items = [
   }
 ];
 
-const App = () => {
+const options = [
+  {
+    label: "The Color Red",
+    value: "red"
+  },
+  {
+    label: "The Color Green",
+    value: "green"
+  },
+  {
+    label: "The Color Blue",
+    value: "blue"
+  }
+];
+export default () => {
   return (
     <div>
-      <Search />
+      <Dropdown options={options} />
     </div>
   );
 };
-export default App;
