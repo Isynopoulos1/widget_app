31d55d21fa7ad2d5ef3ab34fd8d2aaf2b4dfe9eb
Isela <isela.v.alarcon@gmail.com>
2021-06-30 18:22:50 +0200

    Binding a event handler part 1

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    document.body.addEventListener("click", () => {
+      console.log("click!");
+    });
+  }, []);
+
   const renderedOptions = options.map(option => {
     if (option.value === selected.value) {
       return null;
